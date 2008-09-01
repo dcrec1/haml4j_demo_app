dd95d5278b8812afd5529ec28a8ed9d50a5c2695
Diego Carrion <dc.rec1@gmail.com>
2008-09-01 13:42:07 -0300

    new demos

--- src/com/mouseoverstudio/haml4j/demo/DemoController.java
+++ src/com/mouseoverstudio/haml4j/demo/DemoController.java
@@ -9,33 +9,31 @@ import org.vraptor.annotations.Out;
 
 @Component
 public class DemoController {
-	
+
 	@Out
 	private String title;
-	
+
 	@Out
 	private String information;
-	
+
 	@Out
 	private String header1;
-	
+
 	@Out
 	private String header2;
-	
+
 	@Out
 	private List<Entry> entries;
-	
+
+	public String ampersand() {
+		return "ok";
+	}
+
 	public String basic() {
 		title = "I was born in the controller";
 		return "ok";
 	}
-	
-	public String haiku() {
-		title = "Hamlj4 Demonstration";
-		information = "VRaptor urls are sexy, together with Haml even more!";
-		return "ok";
-	}
-	
+
 	public String big() {
 		title = "Haml4j test with a big file";
 		header1 = "Header title number 1";
@@ -44,6 +42,24 @@ public class DemoController {
 		entries.add(new Entry("Title 1", new Date(), "fewgew gew gewggw"));
 		entries.add(new Entry("Title 2", new Date(), "ewr wer t43t43btrn5"));
 		entries.add(new Entry("Title 3", new Date(), "ijt hrt rgrehrhg reg"));
+		return "ok";
+	}
+
+	public String condition() {
+		return "ok";
+	}
+
+	public String escape() {
+		return "ok";
+	}
+
+	public String haiku() {
+		title = "Hamlj4 Demonstration";
+		information = "VRaptor urls are sexy, together with Haml even more!";
+		return "ok";
+	}
+
+	public String loop() {
 		return "ok";
 	}
 
